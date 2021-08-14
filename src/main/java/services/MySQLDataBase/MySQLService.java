01b0d85a5ab6fc0ce83f8ef102fc699d03847ab3
Martín Caruso <38790246+Caruso-Martin@users.noreply.github.com>
2021-08-13 23:55:19 -0300

    Version 2.1 (13/8)

--- src/main/java/services/MySQLDataBase/MySQLService.java
+++ src/main/java/services/MySQLDataBase/MySQLService.java
@@ -1,4 +1,28 @@
 package services.MySQLDataBase;
 
+import java.sql.*;
+
 public class MySQLService {
+    public static void main(String[] args) {
+        MySQLService mySQL = new MySQLService();
+        mySQL.crearConexion();
+    }
+
+    public void crearConexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cafetindb", "root", "root");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCTOS");
+            while (resultSet.next()){
+                String name = resultSet.getString("nombre");
+                System.out.println(name);
+            }
+
+            System.out.println("¡Conexion con cafetinDB exitosa!");
+            statement.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
